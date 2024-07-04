e2d507e6d3c8e319b9c8cb0fba20260c6214027e
sudenursahin <sudynoor@gmail.com>
2024-07-04 16:52:32 +0300

    The project utilizes Entity Framework Core for database operations and follows the MVC architectural pattern.
    This project is a simple Category Management MVC application built with ASP.NET Core. It includes basic CRUD operations  for managing categories. The project structure consists of a Category model, a CategoryController for handling CRUD actions, and Razor views for displaying the data. Users can add, list, update, and delete categories through an intuitive interface designed with Bootstrap.

---
+++ SudyWeb/SudyWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SudyWebApp.Models;
+
+namespace SudyWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+
+
+
+
+            
+        }
+
+        // this simple one line creates the table , then  we need to do something called migration
+        public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // thanks to taking ModelBuilder as a parameter, we see the data
+        {
+            modelBuilder.Entity<Category>().HasData(
+             new Category { Id=1, Name="Sude" , DisplayOrder=1},
+             new Category  { Id =2, Name = "Ertuğrul" , DisplayOrder = 2},
+             new Category { Id = 3, Name = "Goktug" , DisplayOrder = 3}
+              ); // whenever u have an update in ur db , u have to add migration
+        }
+
+
+
+
+
+    }
+}
---
+++ SudyWeb/SudyWebApp/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SudyWebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order" )]
+        [Range(1,100,ErrorMessage="Display order must be between1-100 !!!!!")]
+        public int DisplayOrder { get; set; }
+
+
+
+    }
+}
